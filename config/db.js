4e4255ef2e5a68034d67486e2e4dfd4b6435c4e4
syedrafayjavaid <syedrafayjavaid16@gmail.com>
2023-01-13 19:30:55 +0500

    sdasdasda

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     console.log("The database connection url is", process.env.LOCAL_MONGODB_URI);
     try {
         mongoose.set('strictQuery', false);
-        const connect = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        const connect = await mongoose.connect("mongodb+srv://rafay:rafay2022@cluster0.zbivmmq.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
         console.log(`MongoDB  successfully connected to ${connect.connection.host}`)
     } catch (error) {
         console.log('Connection could not be created, possible cause: ' + error.message);
